2e35ddb30a81b634a3bb8f4096bbefd39a63cd6d
alshimaaelnady <alshimaaelnady@gmail.com>
2020-04-07 08:35:41 +0200

    first commit

---
+++ src/main/java/ShowResult.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+public class ShowResult extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String username= request.getParameter("uName");
+        if(username.equals("Alshimaa")){
+            out.print("valid");
+        }else{
+            out.print("Invalid");
+        }
+
+
+
+    }
+
+}
